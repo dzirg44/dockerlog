c0d9f777680273a9d52ae6068a40c999a5af717d
Oleg Tsymbal <dzirg44@gmail.com>
2020-07-22 14:51:34 +0300

    Init commit

---
+++ backend/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    print('sssss');
+    Log::info('User failed to login.');
+    return view('welcome');
+
+});
